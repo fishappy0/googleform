dcb59b2983ca0864153d7ae9a6dc112cd528fb3f
Fishappy0 <fishappy0@gmail.com>
2022-10-30 21:40:45 +0700

    Changed my mind, not using selenium anymore lol

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
-use thirtyfour::prelude::*;
+use reqwest;
 use tokio;
 
 #[tokio::main]
-async fn main() -> WebDriverResult<()> {
-    let caps = DesiredCapabilities::chrome();
-    let driver = WebDriver::new("http://localhost:9515", caps).await?;
-
-    driver.goto("https://www.google.com").await?;
-    let elem = driver.find(By::Name("q")).await?;
-    elem.send_keys("Hello world").await?;
-
-    let elem_button = elem.find(By::Css("input[type='submit']")).await?;
-    elem_button.click().await?;
-
-    driver.quit().await?;
-    Ok(())
+async fn main() {
+    let client = reqwest::Client::new();
+    let res = client.get("https://www.rust-lang.org").send().await;
+    println!("{:?}", res);
 }
